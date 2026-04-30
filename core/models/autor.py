1681be2ab7a3bd923388161343839537cad52a72
amandastss <amandassantos3103@gmail.com>
2026-04-30 11:54:15 -0300

    feat: customização do Admin

--- core/models/autor.py
+++ core/models/autor.py
@@ -2,12 +2,12 @@ from django.db import models
 
 
 class Autor(models.Model):
-    descricao = models.CharField(max_length=100)
-    email = models.EmailField(max_length=100, null=True, blank=True)  # noqa: F811
+    nome = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100, blank=True, null=True)
 
     def __str__(self):
-        return self.descricao
+        return f'({self.id}) {self.nome}'
 
     class Meta:
-        verbose_name = 'Autor'
-        verbose_name_plural = 'Autores'
+        verbose_name = 'autor'
+        verbose_name_plural = 'autores'

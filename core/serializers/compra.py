1653247d6eebf3e87d23dd13a4f00315199161f9
amandastss <amandassantos3103@gmail.com>
2026-08-14 08:26:10 -0300

    feat: inclusão do status da compra na listagem da compra

--- core/serializers/compra.py
+++ core/serializers/compra.py
@@ -11,3 +11,7 @@ class CompraSerializer(ModelSerializer):
 
 class CompraSerializer(ModelSerializer):  # ruff: ignore[redefined-while-unused]
     usuario = CharField(source='usuario.email', read_only=True)
+
+
+class CompraSerializer(ModelSerializer):  # ruff: ignore[redefined-while-unused]
+    status = CharField(source='get_status_display', read_only=True)
